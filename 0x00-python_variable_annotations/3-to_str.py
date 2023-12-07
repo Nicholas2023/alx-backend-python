9efcb6bd283483f373e8f92a4a7b8e705d1f8857
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-07 22:02:12 +0300

    task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A module that returns a string from floats
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts float arguments to strings
+    """
+    return str(n)
